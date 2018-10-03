c8feef2fa16d3b72666ce2b168d54d70f0c6957e
BlueTRaven <Taylor@adoniram.net>
2018-10-03 13:27:01 -0400

    First commit

---
+++ SGEmulator/EmMain.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SGEmulator
+{
+	public class EmMain
+	{
+		/// <summary>
+		/// Update game loop
+		/// </summary>
+		/// <param name="time">The amount of time since the game has begun</param>
+		/// <param name="deltaTime">The amount of time that has passed since the last frame</param>
+		public void Update(double time, float deltaTime)
+		{
+			for (int i = 0; i < int.MaxValue; i++)
+			{
+
+			}
+		}
+
+		public void Render()
+		{
+		}
+	}
+}
---
+++ SGEmulator/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SGEmulator
+{
+	class Program
+	{
+		private static bool quit;
+
+		static void Main(string[] args)
+		{
+			M68kDecoder decoder = new M68kDecoder();
+
+			
+			Console.ReadKey();
+		}
+
+		static void DoLoop()
+		{
+			EmMain main = new EmMain();
+
+			Stopwatch watch = new Stopwatch();
+
+			double time = 0;
+			double timeStep = 1 / 60;
+
+			double accumulator = 0;
+
+			double currentTime = watch.ElapsedMilliseconds;
+
+			while (!quit)
+			{
+				double nowTime = watch.ElapsedMilliseconds;
+				double frameTime = nowTime - currentTime;
+
+				if (frameTime > 0.25d)
+					frameTime = 0.25d;
+
+				currentTime = nowTime;
+
+				accumulator += frameTime;
+
+				while (accumulator >= timeStep)
+				{
+					float deltaTime = (float)Math.Min(frameTime, timeStep);
+
+					main.Update(time, deltaTime);
+
+					frameTime -= deltaTime;
+					time += deltaTime;
+				}
+			}
+		}
+	}
+}
